84349a7a22f296b1465a5470e0f237b654dd781a
Rafael <154033540+RafaahCruz@users.noreply.github.com>
2026-05-15 20:13:36 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,12 +17,11 @@ try {
 mais uma */
 
 echo "<p>Estado atual do teste: $var_teste";
-<<<<<<< HEAD
+
 echo "<p>Essa é a versão da branch Main";
-=======
+
 
 
 echo "<p>Essa é a versão da branch Implementation.";
 
->>>>>>> Implementation
 ?>
